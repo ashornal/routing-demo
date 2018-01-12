a69b3f1af28b683fa3d905c86e31d28d5b0c88e0
Alex Shornal <shornality@yahoo.com>
2018-01-12 14:51:40 -0500

    added new routes

--- index.php
+++ index.php
@@ -3,16 +3,44 @@
 //Require the autoload file
 require_once('vendor/autoload.php');
 
+
 //Create an instance of the Base Class
 $f3 = Base::instance();
+
+//Set debug level
+$f3->set('DEBUG',3);
+
 
 //Define a default route
 $f3->route('GET /', function()
 {
+    echo '<h1>Routing Demo</h1>';
 
-    $view = new View;
-    echo $view->render
-    ('views/index.html');
+//    $view = new View;
+//    echo $view->render
+//    ('views/index.html');
+}
+);
+
+//Define a page1 route
+$f3->route('GET /page1', function()
+{
+    echo '<h1>This is page 1</h1>';
+
+//    $view = new View;
+//    echo $view->render
+//    ('views/index.html');
+}
+);
+
+//Define a page2 route
+$f3->route('GET /page1/subpage-a', function()
+{
+    echo '<h1>This is page 1, subpage a</h1>';
+
+//    $view = new View;
+//    echo $view->render
+//    ('views/index.html');
 }
 );
 
